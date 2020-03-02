ebe69c753adaef4d54b6db3a9a2a329c57127427
dyb881 <15118995517@163.com>
2020-03-02 22:30:15 +0800

    整理 passport 目录

--- src/based/auth/auth.module.ts
+++ src/based/auth/auth.module.ts
@@ -4,9 +4,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { AccountModule } from '../account/account.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { LocalStrategy } from './local.strategy';
-import { JwtStrategy } from './jwt.strategy';
-import { jwtConstants } from './constants';
+import { LocalStrategy, JwtStrategy, jwtConstants } from './passport';
 
 @Module({
   imports: [
