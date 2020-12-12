7fa85559c0ce616d6143731ea58997d0553b2471
dyb881 <15118995517@163.com>
2020-12-12 14:29:30 +0800

    .

--- src/common/global/config.ts
+++ src/common/global/config.ts
@@ -5,7 +5,7 @@ export const serveConfig: TServeConfig = {
     host: 'localhost',
     port: 3306,
     username: 'root',
-    password: 'root',
+    password: 'Deng3364',
     database: 'nest_serve_db',
   },
   protocol: 'http',
