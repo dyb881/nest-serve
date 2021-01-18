b2660e9e6f2eeddf24013a87043ca2b76fc7787b
dyb881 <15118995517@163.com>
2021-01-19 01:20:00 +0800

    调整模块顺序

--- apps/account/src/app.module.ts
+++ apps/account/src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { configModule, typeOrmModule } from 'config/module';
 import { AccountModule } from './account/account.module';
+import { RoleAdminModule } from './role-admin/role-admin.module';
 import { AccountAdminModule } from './account-admin/account-admin.module';
 import { AccountUserModule } from './account-user/account-user.module';
-import { RoleAdminModule } from './role-admin/role-admin.module';
 
 @Module({
   imports: [
     configModule('account'),
     typeOrmModule(),
     AccountModule,
+    RoleAdminModule,
     AccountAdminModule,
     AccountUserModule,
-    RoleAdminModule,
   ],
 })
 export class AppModule {}
