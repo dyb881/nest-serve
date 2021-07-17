39d0a07d0976bd029d77c66931215901c9061e77
dyb881 <15118995517@163.com>
2021-07-17 14:01:04 +0800

    编辑文档

--- apps/account/src/main.ts
+++ apps/account/src/main.ts
@@ -4,12 +4,17 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { LoggerService } from '@app/public-module';
 
+import requestIp from 'request-ip';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
 
+  // 获取客户端真实IP
+  app.use(requestIp.mw());
+
   // 获取配置
   const configService = app.get(ConfigService);
-  const port = configService.get('serve');
+  const port = configService.get('serve.port');
 
   await app.listen(port);
 
