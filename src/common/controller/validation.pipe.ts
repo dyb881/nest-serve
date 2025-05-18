0b0c53586cf40effd8dec5cfa2db549862433d56
archergu <649601376@qq.com>
2025-05-18 14:30:40 +0800

    fix: add transform to validation pipe

--- src/common/controller/validation.pipe.ts
+++ src/common/controller/validation.pipe.ts
@@ -1,13 +1,13 @@
 import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer'
 import { validate } from 'class-validator';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform {
   constructor(private readonly Dto: new (...args: any[]) => object) {}
 
-  async transform(value: any, metadata: ArgumentMetadata) {
-    const obj = new this.Dto();
-    Object.assign(obj, value);
+  async transform(value: any, _: ArgumentMetadata) {
+    const obj = plainToInstance(this.Dto, value)
     const errors = await validate(obj, { stopAtFirstError: true });
     if (errors.length > 0) {
       const message = errors.map((i) => i.constraints?.matches).filter(Boolean);
